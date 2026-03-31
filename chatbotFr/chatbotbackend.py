28b0af5afb342d368969380d2ee77ca290170e6f
bhavya sachula <bhavyasachula07@gmail.com>
2026-03-31 22:37:36 +0530

    fix:streaming effect , ram memoy

---
+++ chatbotFr/chatbotbackend.py
@@ -0,0 +1,28 @@
+from langgraph.graph import START,END
+from langgraph.graph import StateGraph,START,END
+from langgraph.graph.message import add_messages
+from typing import TypedDict,Annotated,Literal
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.messages import BaseMessage,HumanMessage,SystemMessage
+from langgraph.checkpoint.memory import MemorySaver # Using this for Persistence
+load_dotenv()
+
+class Chatbotstate(TypedDict):
+    messages:Annotated[list[BaseMessage],add_messages]
+
+checkpointer = MemorySaver() # call the function memorysaver() 
+
+graph = StateGraph(Chatbotstate)
+
+llm = ChatGroq(model="openai/gpt-oss-120b")
+
+def chatbot(state:Chatbotstate):
+    messages = state['messages']
+    res =llm.invoke(messages)
+    return {'messages':[res.content]}
+
+graph.add_node("Chatbot",chatbot)
+graph.add_edge(START,"Chatbot")
+graph.add_edge("Chatbot",END)
+chatbot = graph.compile(checkpointer=checkpointer) 
