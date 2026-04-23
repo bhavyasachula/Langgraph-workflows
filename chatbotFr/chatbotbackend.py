2f52e00c6d90811063b83e3c11eedb595ed45b67
bhavya sachula <bhavyasachula07@gmail.com>
2026-04-23 22:45:25 +0530

    added a getstockprice tool in chatbotFr backend

--- chatbotFr/chatbotbackend.py
+++ chatbotFr/chatbotbackend.py
@@ -6,12 +6,38 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from langchain_core.messages import BaseMessage,HumanMessage,SystemMessage,AIMessage
 from langgraph.checkpoint.sqlite import SqliteSaver # Using this for Persistence
+from langgraph.prebuilt import ToolNode , tools_condition
+from langchain_core.tools import tool
+#from langchain_community.tools import DuckDuckGoSearchRun
+#from langchain_community.tools import DuckDuckGoSearchResults
 import sqlite3
 import os
-os.environ['LANGCHAIN_PROJECT'] = "LANGGRAPh-LANGSMITH" # Set the LANGCHAIN_PROJECT environment variable
+import requests
+os.environ['LANGCHAIN_PROJECT'] = "LANGGRAPH-LANGSMITH" # Set the LANGCHAIN_PROJECT environment variable
 
 load_dotenv()
+
+llm = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct")
+
+@tool
+def getstockprice_for_America_tool(symbol:str):
+    """Fetch a latest stock price of America(US) for a given stock symbol eg(AAPL, MSFT,TSLA)
+        using alpha vantage api. 
+    """
+    url =f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={os.getenv('ALPHAVANTAGE_API_KEY')}"
+    r = requests.get(url)
+    return r.json()
+
+
+
+tools = [getstockprice_for_America_tool]
+
+llm_tools = llm.bind_tools(tools) # Bind the tools to the llm so that it can use them when needed.
+
+tool = ToolNode(tools) # Create a ToolNode with the bound tools.
+
 conn = sqlite3.connect(database="chatbot.db",check_same_thread=False)
+
 class Chatbotstate(TypedDict):
     messages:Annotated[list[BaseMessage],add_messages]
 
@@ -19,22 +45,31 @@ checkpointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(Chatbotstate)
 
-llm = ChatGroq(model="openai/gpt-oss-120b")
 
 def chatbot(state:Chatbotstate):
     messages = state['messages']
-    res =llm.invoke(messages)
-    return {'messages': [AIMessage(content=res.content)]}
+    res =llm_tools.invoke(messages)
+    return {'messages': [res]}
 
 graph.add_node("Chatbot",chatbot)
+graph.add_node("tools",tool) # Add the tool node to the graph
 graph.add_edge(START,"Chatbot")
-graph.add_edge("Chatbot",END)
+graph.add_conditional_edges("Chatbot",tools_condition)
+graph.add_edge("tools", "Chatbot") 
+
+config = {
+    "configurable": {
+        "thread_id": "some_unique_id"
+    }
+}
 chatbot = graph.compile(checkpointer=checkpointer) 
 
-def retrieval_of_threads():
-    allthreads = {
-    checkpoint.config['configurable']['thread_id']
-    for checkpoint in checkpointer.list(None) 
-    }
-     
-    return list(allthreads)
+response=chatbot.invoke({"messages":[HumanMessage(content="What is the latest stock price of tsla in america?")]},config=config) # Example invocation to test the chatbot with a human message.
+print(response["messages"][-1].content) 
+
+# def retrieval_of_threads():
+#     allthreads = {
+#     checkpoint.config['configurable']['thread_id']
+#     for checkpoint in checkpointer.list(None) 
+#     }
+#     return list(allthreads)
