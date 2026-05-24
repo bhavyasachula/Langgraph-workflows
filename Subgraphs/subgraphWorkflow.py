7be8f42af55d519406de9d94f753dc317f7d9c2b
bhavya sachula <bhavyasachula07@gmail.com>
2026-05-24 23:46:14 +0530

    subgraph in shared state

--- Subgraphs/subgraphWorkflow.py
+++ Subgraphs/subgraphWorkflow.py
@@ -8,5 +8,28 @@ load_dotenv()
 class ParentState(TypedDict):
     question:str
     answer_eng:str
-    answer_hindi:str
+    answer_hin:str
+
+parentLLm = ChatGroq(model="openai/gpt-oss-120b")
+subgraphLLm = ChatGroq(model="openai/gpt-oss-120b")
+
+def translate_text(state:StateGraph):
+    
+    prompt = f"""Translate this following text into hindi language
+    keep it natural , Do not add anything extra.
+    
+    text:{state['answer_eng']}
+    """
+    answer = subgraphLLm.invoke(prompt).content
+
+    return {'answer_hindi':answer}
+
+subgraph_builder = StateGraph(ParentState);
+
+subgraph_builder.add_node("translate_text",translate_text)
+
+subgraph_builder.add_edge(START,"translate_text")
+subgraph_builder.add_edge("translate_text",END)
+
+subgraph = subgraph_builder.compile()
 
